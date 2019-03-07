d5eef6799601dd8c9c192ecfccaf7e4bac3a4a2c
acl <acl447@nyu.edu>
2019-03-07 14:01:40 -0700

    edits march 7

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,15 +13,32 @@ $(document).ready(function () {
 
     var losses = 0;
 
+    var counter1 = 0;
+
+    function reset() {
+
+        counter1 = 0;
+        totalScore = 0;
+
+        $("#totalScore").text(totalScore);
+
+        var targetNumber = Math.floor(Math.random() * 102) + 19;
+        
+        console.log(targetNumber);
+
+        $("#target-number").text(targetNumber);
+
+
+    }
+
+    var randomNumber = Math.floor(Math.random() * 12) + 1;
+
+    console.log(randomNumber);
 
 
     $("#crystal-1").on("click", function () {
 
-        var counter1 = 0;
 
-        var randomNumber = Math.floor(Math.random() * 12) + 1;
-
-        console.log(randomNumber);
 
 
         if (targetNumber > counter1) {
@@ -30,17 +47,17 @@ $(document).ready(function () {
 
             console.log(counter1);
 
-            totalScore += counter1;
+            totalScore = counter1;
 
             console.log(totalScore);
 
-            //document.getElementById("totalScore").innerHTML = totalScore;
-
-            $("#totalScore").text = totalScore;
+            $("#totalScore").text(totalScore);
 
         }
 
         else if (targetNumber === counter1) {
+
+            console.log(counter1);
 
             targetReached = true;
 
@@ -48,10 +65,14 @@ $(document).ready(function () {
 
             wins++;
 
-            $("#winsNumber").text = wins;
+            $("#winsNumber").text(wins);
+
+            reset();
 
 
         } else if (targetNumber < counter1) {
+
+            console.log(counter1);
 
             targetReached = false;
 
@@ -60,23 +81,29 @@ $(document).ready(function () {
 
             losses++;
 
-            $("#lossesNumber").text = losses;
-        };
+            $("#lossesNumber").text(losses);
 
+            reset();
 
-
-
-
-
-
-
-
-
-
-
+        }
 
 
     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
